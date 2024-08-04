eb580292c39bddd05b6d7322e403e514cb929a27
Chelsea Liu <chelsealiu@oomii.com>
2024-08-04 21:13:42 +0800

    feat(vscode): add launch and task configurations for .NET Core development to streamline the development process fix(Model.cs): update Blog.Url property to be nullable and make Title and Content properties required for better data integrity

--- Model.cs
+++ Model.cs
@@ -25,7 +25,7 @@ public class BloggingContext : DbContext
 public class Blog
 {
     public int BlogId { get; set; }
-    public string Url { get; set; }
+    public string? Url { get; set; }
 
     public List<Post> Posts { get; } = new();
 }
@@ -33,9 +33,9 @@ public class Blog
 public class Post
 {
     public int PostId { get; set; }
-    public string Title { get; set; }
-    public string Content { get; set; }
+    public required string Title { get; set; }
+    public required string Content { get; set; }
 
     public int BlogId { get; set; }
-    public Blog Blog { get; set; }
+    public Blog Blog { get; set; } = new();
 }
